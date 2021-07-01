c26ec07ceaa4a7ac3554168d42b6057d0d690d8e
irritas <brianliu9606@gmail.com>
2021-06-30 17:10:39 -0700

    sets up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Router className="App">
+      <Switch>
+        <Route exact path='/' render={() =>
+          <HomePage />
+        }/>
+      </Switch>
+    </Router>
   );
 }
 
