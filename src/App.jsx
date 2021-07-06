8527e33fd9f1f0b8e3e914b5bd919ad285794618
irritas <brianliu9606@gmail.com>
2021-07-06 15:31:28 -0700

    fixes imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
-  return (
-    <Router className="App">
-      <Switch>
-        <Route exact path='/' render={() =>
-          <HomePage />
-        }/>
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router className='App'>
+			<Switch>
+				<Route exact path='/' render={() => <HomePage />} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
