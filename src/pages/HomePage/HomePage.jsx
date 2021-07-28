246697f3c305416da3425ec8276a0690c4313ed7
irritas <brianliu9606@gmail.com>
2021-07-27 20:54:27 -0700

    stubs up header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 import './HomePage.css';
 
 export default function HomePage(props) {
 	return <div>
-		<div></div>
+		<Header />
 	</div>;
 }
